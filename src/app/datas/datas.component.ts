af78690c797b6c0105afca5c5e6bf88f28cebc00
karthi9500 <karthikeyan.d@finstein.ai>
2022-10-05 15:53:33 +0530

    initial commit

---
+++ src/app/datas/datas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datas',
+  templateUrl: './datas.component.html',
+  styleUrls: ['./datas.component.scss']
+})
+export class DatasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
